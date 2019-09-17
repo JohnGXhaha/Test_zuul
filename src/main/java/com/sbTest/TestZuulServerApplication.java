bd9bf7deab25b4bad16e5f2e40cc0835043b15a9
JonnGXhaha <376419326@qq.com>
2019-09-17 22:07:42 +0800

    zuul网关测试

---
+++ src/main/java/com/sbTest/TestZuulServerApplication.java
@@ -0,0 +1,15 @@
+package com.sbTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class TestZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestZuulServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbTest/TestZuulServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sbTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestZuulServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
